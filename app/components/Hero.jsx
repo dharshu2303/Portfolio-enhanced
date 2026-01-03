4ef9f417372c2508c8ffb945b59b3fccaf9d29c0
DHARSHINI PRIYA A <dharshinipriya.a426@gmail.com>
2026-01-04 02:55:21 +0530

    Adjust Hero component alignment for better layout

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -67,7 +67,7 @@ export default function Hero() {
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, delay: 0.3 }}
-            className="flex justify-center md:justify-end"
+            className="flex justify-center"
           >
             <div className="profile-img relative">
               <Image
